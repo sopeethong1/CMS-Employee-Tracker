54dfb911f73d61441c9257b2bab22001144e103c
Sopee Thong <tsopee@gmail.com>
2021-04-19 23:05:55 -0400

    adding values to employee

---
+++ seed.sql
@@ -0,0 +1,13 @@
+
+USE sopee_companyDB;
+
+/* Insert 3 Rows into your new table */
+INSERT INTO employee(
+    id, 
+    name)
+VALUES (1, Sopee),
+(2, Tommy),
+(3, Labat),
+(4, Sandy),
+(5, Sebastian),
+(6, Asher);

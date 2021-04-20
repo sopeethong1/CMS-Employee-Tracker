f0ce146ba97b67dfc5b78cd2302097a4592ed77f
Sopee Thong <tsopee@gmail.com>
2021-04-19 22:53:10 -0400

    creating database and tables

---
+++ schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS sopee_companyDB;
+CREATE database sopee_companyDB;
+
+USE sopee_companyDB;
+
+CREATE TABLE department(
+  id INT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  name INT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL
+);
+
+CREATE TABLE EMPLOYEE (
+  id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
+);
+SELECT * FROM department;
+
+SELECT * from role;
+
+SELECT * from employee;

2920100751c345f6fdd55f39d0997d49d51d1047
Sopee Thong <tsopee@gmail.com>
2021-04-20 21:59:26 -0400

    organizing files in proper folders

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,2 +1,17 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: 'root',
+  
+    // Be sure to update with your own MySQL password!
+    password: 'Target03!',
+    database: 'top_songsDB',
+  });
+  
